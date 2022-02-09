9ebe0ae96ed5e33d94b012cb534875f3dddd4ef8
github-deniza235 <lobashova.mariya@mail.ru>
2022-02-09 19:44:37 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let newDecodeMorse = [];
+    let decodeWords =  [];
+    let resultDecode = '';
+    
+    for(let i = 0; i < expr.length; i += 10) {
+        newDecodeMorse.push(expr.slice(i, i + 10));
+    }
+
+    for(let elem of newDecodeMorse) {
+        let words = [];
+        if(elem === '**********') {
+            words.push(' ');
+        } else {
+            for(let i = 0; i < elem.length; i += 2) {
+                el = elem.slice(i, i + 2);
+                if (el === '10') {
+                    words.push('.')
+                } else if(el === '11') {
+                    words.push('-');
+                }
+                
+            }
+        }
+        decodeWords.push(words.join(''));
+        
+    }
+
+    for(let el of decodeWords) {
+        if( el === ' ') {
+            resultDecode += ' ';
+        } else {
+            resultDecode += MORSE_TABLE[el];
+        }
+    }
+
+    return resultDecode;
+    
+    
 }
 
 module.exports = {
